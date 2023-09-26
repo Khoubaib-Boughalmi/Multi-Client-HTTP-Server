6a135cfd390bacc7587e2f846607af1a3a1f45c2
Khoubaib-Boughalmi <khoubaib.kb99@gmail.com>
2023-09-26 13:26:13 +0100

    fixed unclosed clientSocketFd

--- serverUtils.c
+++ serverUtils.c
@@ -23,41 +23,43 @@ void receiveIncommingRequestAndRespond (int clientSocketFd) {
     char httpReq[32768] = "";
     char *getResponse = "HTTP/1.1 200 OK\r\nDate: Sat, 24 Sep 2023 12:00:00 GMT\r\nContent-Type: text/html\r\nConnection: keep-alive\r\n\r\n<!DOCTYPE html>\r\n<html>\r\n<head>\r\n<title>Sample Page</title>\r\n<style>body {background-color: #f0f0f0;margin: 0;padding: 0;}h1 {color: blue;}p {color: red;}</style>\r\n</head>\r\n<body>\r\n<h1>Hello, World!</h1>\r\n<p>This is a sample page.</p>\r\n</body>\r\n</html>\r\n";
     char *postResponse = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<html><body><h1>POST response</h1></body></html>";
-    while (1)
-    {
-        memset(httpReq, 0, sizeof(httpReq));
-        int bytesRecv = recv(clientSocketFd, httpReq, sizeof(httpReq), 0);
-        if(!bytesRecv || bytesRecv < 0) {
-            if(bytesRecv < 0)
-                perror("recv error");
-            else
-            {
-                close(clientSocketFd);
-                printf("Client closed connection\n");
-            }
-            break;
+
+    memset(httpReq, 0, sizeof(httpReq));
+    int bytesRecv = recv(clientSocketFd, httpReq, sizeof(httpReq), 0);
+    if(!bytesRecv || bytesRecv < 0) {
+        if(bytesRecv < 0)
+        {
+            printf("bytesRecv: %d\n", bytesRecv);
+            perror("recv error");
         }
-        printf("Client's Request: %s", httpReq);
-        char *getReq = strstr(httpReq, "GET");
-        char *postReq = strstr(httpReq, "POST");
-        int bytesSent = 0;
-        if(getReq != NULL) {
-            bytesSent = send(clientSocketFd, getResponse, strlen(getResponse), 0);
+        else
+        {
+            close(clientSocketFd);
+            printf("Client closed connection\n");
         }
-        else if(postReq != NULL) {
-            bytesSent = send(clientSocketFd, postResponse, strlen(postResponse), 0);
-        }
-        else {
-            printf("Unknown Request\n");
-            bytesSent = -1;
-        }
-        if(bytesSent < 0) {
-            perror("send error");
-            break;
-        }
-        printf("send success\n");
-        close(clientSocketFd);
+        return ;
     }
+    printf("Client FD %d\n", clientSocketFd);
+    printf("Client's Request: %s", httpReq);
+    char *getReq = strstr(httpReq, "GET");
+    char *postReq = strstr(httpReq, "POST");
+    int bytesSent = 0;
+    if(getReq != NULL) {
+        bytesSent = send(clientSocketFd, getResponse, strlen(getResponse), 0);
+    }
+    else if(postReq != NULL) {
+        bytesSent = send(clientSocketFd, postResponse, strlen(postResponse), 0);
+    }
+    else {
+        printf("Unknown Request\n");
+        bytesSent = -1;
+    }
+    if(bytesSent < 0) {
+        perror("send error");
+        return;
+    }
+    printf("send success\n");
+    close(clientSocketFd);
 }
 
 void *startAcceptingIncomingConnections(int serverSocketFd) {
@@ -69,8 +71,13 @@ void *startAcceptingIncomingConnections(int serverSocketFd) {
             perror("accept error");
             return (NULL);   
         }
+        printf("Client FD %d\n", acceptedSocket->clientSocketFd);
         pthread_t thread_id;
-        pthread_create(&thread_id, NULL, receiveAndRespond, &(acceptedSocket->clientSocketFd));
+        pthread_attr_t attr;
+        pthread_attr_init(&attr);
+        // Set the thread as detached
+        pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
+        pthread_create(&thread_id, &attr, receiveAndRespond, &(acceptedSocket->clientSocketFd));
     }
     return (NULL);   
 }
