4d48a0df11e8a12061ac8ba04c9201b6730ae912
Khoubaib-Boughalmi <khoubaib.kb99@gmail.com>
2023-09-26 22:28:56 +0100

    fixed 2nd req bug in client

--- client.c
+++ client.c
@@ -4,26 +4,26 @@
 //GET / HTTP/1.1\r\nHost: www.boobs.com\r\n\r\n
 
 int main(void) {
-    int sockfd;
-    sockfd = createIPV4Socket();
-    if (sockfd < 0) {
-        perror("socket error");
-        return (-1);
-    } 
-    struct sockaddr_in *addr = createIPV4Address("127.0.0.1", 8080);
-    int res = connectSocket(sockfd, addr);
-    if (res < 0) {
-        free(addr);
-        perror("connect error");
-        return (-1);
-    }
-    printf("connect success\n");
-
-    char *httpReq = NULL;
-    int httpReqRead = 0;
-    size_t httpReqSize = 0;
     while (1)
     {
+        int sockfd;
+        sockfd = createIPV4Socket();
+        if (sockfd < 0) {
+            perror("socket error");
+            return (-1);
+        } 
+        struct sockaddr_in *addr = createIPV4Address("127.0.0.1", 8080);
+        int res = connectSocket(sockfd, addr);
+        if (res < 0) {
+            free(addr);
+            perror("connect error");
+            return (-1);
+        }
+        printf("connect success\n");
+
+        char *httpReq = NULL;
+        int httpReqRead = 0;
+        size_t httpReqSize = 0;
         printf("HTTP Request: ");
         httpReqRead = getline(&httpReq, &httpReqSize, stdin);
         if(httpReqRead < 0) {
@@ -47,8 +47,8 @@ int main(void) {
         printf("HTTP response: %s\n", httpRes);
         free(httpReq);
         httpReq = NULL;
+        close(sockfd);
+        free(addr);
     }
-    close(sockfd);
-    free(addr);
     return (0);
 }
